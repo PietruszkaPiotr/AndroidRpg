be0446221f357c34274b72312c6fd12717956298
Damian <damianowczarczyk19@gmail.com>
2019-06-11 02:50:58 +0200

    Added simple menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void OnQuitClick()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
